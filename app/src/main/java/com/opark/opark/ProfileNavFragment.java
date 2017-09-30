0a5dc698324c22fb095e107f60b74f10ccf270b9
Yz <yz_lim95@hotmail.com>
2017-09-30 23:31:14 +0100

    Rearrange Layout, added Layout Id, Waiting to download objbytestream from Storage Created new fragment layout ready for feedback fragment. Resized app bar layout . Subject to change

--- app/src/main/java/com/opark/opark/ProfileNavFragment.java
+++ app/src/main/java/com/opark/opark/ProfileNavFragment.java
@@ -9,6 +9,7 @@ import android.support.v4.widget.DrawerLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by Yz on 21-Sep-17.
@@ -16,12 +17,22 @@ import android.view.ViewGroup;
 
 public class ProfileNavFragment extends Fragment {
 
-
+    private TextView firstName, lastName, phoneNum;
+    private TextView carColour, carBrand, carModel, carPlate;
+    private TextView firstLine, secondLine, city, postcode, countryState;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState) {
         // Defines the xml file for the fragment
-        return inflater.inflate(R.layout.profile_nav_fragment, parent, false);
+        View view=  inflater.inflate(R.layout.profile_nav_fragment, parent, false);
+
+        firstName = (TextView) view.findViewById(R.id.first_name_edit);
+        firstName.setText("");
+
+        return view;
+
+
+
     }
     // This event is triggered soon after onCreateView().
     // Any view setup should occur here.  E.g., view lookups and attaching view listeners.
