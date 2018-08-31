aaa614d283bc87e34ffcbd7a8adbf6b50961e215
Yz <yz_lim95@hotmail.com>
2018-09-01 01:30:54 +0800

    ViewPage added

---
+++ app/src/main/java/com/opark/opark/login_auth/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package com.opark.opark.login_auth;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.opark.opark.OtherInfoFragment;
+import com.opark.opark.login_auth.PhoneAuth;
+import com.opark.opark.NameEmailFragment;
+
+
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    public ViewPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    
+
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new EmailLogin();
+        } else {
+            return new PhoneAuth();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
